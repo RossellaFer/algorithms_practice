d76cc0d27e3af4cffc928777a9e59d275e66fc74
Rossella Ferrandino <39148794+RossellaFer@users.noreply.github.com>
2022-11-11 18:04:56 +0100

    first algorithm

---
+++ twoSum/twoSum.js
@@ -0,0 +1,13 @@
+// brute force solution, looping over two
+
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if(target === nums[i] + nums[j]) {
+                return [i, j]
+            }
+        }
+    }
+};
+
+twoSum([2,7,11,15], 9)

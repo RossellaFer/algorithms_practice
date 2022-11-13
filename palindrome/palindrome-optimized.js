afec384262cca0dfbd489f691fac4fae60a0020f
Rossella Ferrandino <39148794+RossellaFer@users.noreply.github.com>
2022-11-13 16:57:53 +0100

    Create palindrome-optimized.js

---
+++ palindrome/palindrome-optimized.js
@@ -0,0 +1,15 @@
+var isPalindrome = function(x) {
+    let reverse = 0;
+    let copy = x;
+    
+    while(copy > 0) {
+        let digit = copy % 10;
+        reverse = reverse * 10 + digit;
+        copy = Math.floor(copy / 10);
+    }
+    
+    return x === reverse;
+}
+
+
+console.log(isPalindrome(121));

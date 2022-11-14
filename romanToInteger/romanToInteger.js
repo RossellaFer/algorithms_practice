99e898425426c5932cc16e02b20e75e98a560734
Rossella Ferrandino <39148794+RossellaFer@users.noreply.github.com>
2022-11-14 22:06:41 +0100

    refactored solution

--- romanToInteger/romanToInteger.js
+++ romanToInteger/romanToInteger.js
@@ -1,34 +1,25 @@
-var romanToInt = function(s) {
-    let integerValue = 0;
-      const conversionChart = {
-        I: 1,
-        V: 5,
-        X: 10,
-        L: 50,
-        C: 100,
-        D: 500,
-        M: 1000,
-      };
-    
-      for (let i = s.length - 1; i >= 0; i--) {
-        const char = s[i];
-        const nextChar = s[i + 1];
-        if (
-          (char === 'I' && nextChar === 'X') ||
-          (char === 'I' && nextChar === 'V') ||
-          (char === 'X' && nextChar === 'L') ||
-          (char === 'X' && nextChar === 'C') ||
-          (char === 'C' && nextChar === 'D') ||
-          (char === 'C' && nextChar === 'M')
-        ) {
-          integerValue -= conversionChart[char];
-        } else {
-          integerValue += conversionChart[char];
-        }
-      }
-    
-      return integerValue;
-    };
-    
-    console.log(romanToInt("MCMXCIV"))
-    
+// previous solution was 94ms, this is 87ms
+
+// move this out as it could be used for other functions
+const conversionChart = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+const romanToInt = function (s) {
+  let integerValue = 0;
+
+  for (let i = 0; i <= s.length - 1; i++) {
+    const char = s[i];
+    const nextChar = s[i + 1];
+    conversionChart[char] < conversionChart[nextChar]
+      ? (integerValue -= conversionChart[char])
+      : (integerValue += conversionChart[char]);
+  }
+  return integerValue;
+};

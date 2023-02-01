4b0c2a2cfec0eb2a38c33d996ef38e77df28557f
Rossella Ferrandino <39148794+RossellaFer@users.noreply.github.com>
2023-02-01 22:58:34 +0100

    tag blogs

--- .eleventy.js
+++ .eleventy.js
@@ -5,24 +5,20 @@ const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/");
   eleventyConfig.addPassthroughCopy("src/css/");
-
   eleventyConfig.addWatchTarget("src/css/");
-  eleventyConfig.addShortcode("Card", Card);
 
   eleventyConfig.addPlugin(syntaxHighlight);
+
+  eleventyConfig.addCollection('posts', function(collectionApi) {
+    return collectionApi.getFilteredByGlob('src/blog/posts/**/*.md');
+  })
 
   eleventyConfig.addFilter("readablePostDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, {
         zone: "Europe/Rome",
     }).setLocale('en').toLocaleString(DateTime.DATE_FULL);
   });
-
-  eleventyConfig.addFilter("randomItem", (arr) => {
-      arr.sort(() => {
-        return 0.5 - Math.random();
-      });
-      return arr.slice(0, 1);
-    });
+  
     return {
       dir: {
         input: "src",
